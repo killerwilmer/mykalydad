158d185755ad103d92914b755e892379d67512df
mildred <mildred@mildred-PC>
2012-09-25 01:39:13 -0500

    made forms

---
+++ default/app/controllers/addpregunta_controller.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of addpregunta
+ *
+ * @author mildred
+ */
+class AddpreguntaController extends AppController {
+    public function index(){
+        
+    }
+}
+
+?>
