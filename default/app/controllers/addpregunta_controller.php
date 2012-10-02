b653d1fa4b31e24443fe80f925faaa17dd28fac6
wilmerarteaga <wilmerarteaga@192.168.0.17>
2012-10-01 21:57:53 -0500

    new add pregunta

--- default/app/controllers/addpregunta_controller.php
+++ default/app/controllers/addpregunta_controller.php
@@ -16,29 +16,54 @@ class AddpreguntaController extends AppController {
         if (Input::hasPost('pregunta')) {
 
             $numerocampos = Input::request('numerocampos');
+
             if ($numerocampos == null) {
                 $numerocampos = 1;
             }
 
-            for ($index = 1; $index <= $numerocampos; $index++) {
-                $campo = Input::post('campo' . $index);
-                $tipocampo = Input::post('tipocampo' . $index);
-                Flash::notice('campo: ' . $campo . ' tipocampo: ' . $tipocampo);
-            }
 
-            Load::model('pregunta');
+            Load::models('pregunta', 'campopregunta');
             $obj = new Pregunta(Input::post('pregunta'));
             if ($obj->guardar(Input::post('pregunta'))) {
                 Flash::valid('La Pregunta Ha Sido Agregado Exitosamente...!!!');
             } else {
                 Flash::warning('No se Pudieron Guardar los Datos...!!!');
             }
+
+
+            $ob = new Campopregunta();
+
+            try {
+
+                $ob->begin();
+
+                for ($index = 1; $index <= $numerocampos; $index++) {
+
+                    $campo = Input::post('campo' . $index);
+                    $tipocampo = Input::post('tipocampo' . $index);
+
+                    Flash::notice('campo: ' . $campo . ' tipocampo: ' . $tipocampo);
+
+                    $ob->nombrecampo = $campo;
+                    $ob->pregunta_id = 1; //recibir arriba
+                    $ob->tipocampo_id = $tipocampo;
+                    if (!$ob->save()) {
+                        $ob->rollback();
+                        Flash::notice("No se pudo grabar");
+                    } else {
+                        $ob = new Campopregunta();
+                    }
+                }
+                $ob->commit();
+            } catch (Exception $e) {
+                $ob->sql("ROLLBACK");
+            }
             /* Antes
-            if ($obj->save()) {
-                Flash::notice('Pregunta registra satisfactoriamente');
-            } else {
-                Flash::error('Error al registar la pregunta.');
-            }*/
+              if ($obj->save()) {
+              Flash::notice('Pregunta registra satisfactoriamente');
+              } else {
+              Flash::error('Error al registar la pregunta.');
+              } */
         }
     }
 
