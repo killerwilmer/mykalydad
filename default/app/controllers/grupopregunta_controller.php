06159838bdae4ab74011392dd224213fb4698c8c
Admin <Admin@192.168.2.206>
2012-10-04 15:32:47 -0500

    creado grupopregunta

---
+++ default/app/controllers/grupopregunta_controller.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of grupopregunta_controller
+ *
+ * @author Admin
+ */
+class GrupopreguntaController extends ScaffoldController {
+    public $model = 'grupopregunta';
+}
+
+?>
---
+++ default/app/models/grupopregunta.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of grupopregunta
+ *
+ * @author Admin
+ */
+class grupopregunta extends ActiveRecord {
+    //put your code here
+}
+
+?>
