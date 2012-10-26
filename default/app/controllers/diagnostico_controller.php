548c1423ab887a9e8cb74026cb1ca6bb9641abea
Admin <Admin@192.168.2.206>
2012-10-26 12:02:14 -0500

    listando datos.

--- default/app/controllers/diagnostico_controller.php
+++ default/app/controllers/diagnostico_controller.php
@@ -10,9 +10,18 @@
  *
  * @author Admin
  */
+Load::model('car');
+
 class DiagnosticoController extends ApplicationController {
-    function index(){
-        
+
+    function index($fac_fac_id = 1) {
+        $car = new Car();
+        $this->lista = $car->find("conditions: fac_id=$fac_fac_id order by id asc");
+
+        if (Input::hasPost("fac_fac_id")) {
+            $this->redirect("diagnostico/index/" . Input::post("fac_fac_id"));
+            Session::set("fac_fac_id", Input::post("fac_fac_id"));
+        }
     }
 }
 
